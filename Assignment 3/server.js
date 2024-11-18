1daba55963c9d2159fce219f9bd676fcd5fdbf50
AreejFatim124 <Sp23-BSE-041@cuilahore.edu.pk>
2024-11-18 21:02:30 +0500

    main

---
+++ Assignment 3/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+let app = express();
+var expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/cv", (req, res) => {
+  res.render("CV");
+});
+
+app.get("/", (req, res) => {
+  res.render("GourmetFoods");
+});
+
+app.listen(5500, () => {
+  console.log("Server started at localhost:5500");
+});
