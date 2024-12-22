85437b2ef49fde0ce95d460621934a0ac007ce59
AreejFatim124 <Sp23-BSE-041@cuilahore.edu.pk>
2024-12-22 14:47:33 +0500

    labtask3 done

--- Admin Panel/models/category.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-let Categoryschema = new mongoose.Schema({
-    categoryName : String,
-})
-
-let Category = mongoose.model("Category",Categoryschema);
-module.exports = Category
